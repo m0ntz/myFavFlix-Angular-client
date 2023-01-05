9c3bfb40c3f38f2692ff51e6d875ea638b896eab
m0ntz <109100764+m0ntz@users.noreply.github.com>
2023-01-05 16:07:57 +0100

    added Login Form

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -4,25 +4,25 @@ import { UserRegistrationService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'app-user-registration-form',
-  templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss'],
+  selector: 'app-user-login-form',
+  templateUrl: './user-login-form.component.html',
+  styleUrls: ['./user-login-form.component.scss'],
 })
-export class UserRegistrationFormComponent implements OnInit {
-  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' };
 
   constructor(
     public fetchApiData: UserRegistrationService,
-    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {}
 
-  registerUser(): void {
-    this.fetchApiData.userRegistration(this.userData).subscribe(
+  loginUser(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe(
       (result) => {
-        // Logic for a successful user registration
+        // Logic for a successful user login
         this.dialogRef.close(); // This will close the modal on success!
         this.snackBar.open(result, 'OK', {
           duration: 2000,
